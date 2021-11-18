8a7f1a66a33230b1d30a5cdfa5bbb94ac0f1f8ea
John paul Adobas <johnpaul.adobas@edu.sait.ca>
2021-11-18 01:42:44 -0700

    tweaks

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,7 +1,7 @@
 <template>
 <div class= "flex justify-center flex-row mt-8 gap-1">
-<nuxt-img src="images/images1.jpg" sizes="sm:100vw md:50vw lg:400px" />
-<nuxt-img src="images/images2.jpg" sizes="sm:100vw md:50vw lg:400px" />
-<nuxt-img src="images/images3.jpg" sizes="sm:100vw md:50vw lg:400px" />
+<nuxt-img src="./images/images1.jpg" sizes="sm:100vw md:50vw lg:400px" />
+<nuxt-img src="./images/images2.jpg" sizes="sm:100vw md:50vw lg:400px" />
+<nuxt-img src="./images/images3.jpg" sizes="sm:100vw md:50vw lg:400px" />
 </div>
 </template>
