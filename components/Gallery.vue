75d965736c39f7334340c3113848a7245607d0e1
John paul Adobas <johnpaul.adobas@edu.sait.ca>
2021-11-18 01:34:56 -0700

    added some images

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,8 +1,7 @@
 <template>
-<div>
-<nuxt-img 
-src="https://images.unsplash.com/photo-1578632767115-351597cf2477?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
-
-/>
+<div class= "flex justify-center flex-row mt-8 gap-1">
+<nuxt-img src="/images/images1.jpg" sizes="sm:100vw md:50vw lg:400px" />
+<nuxt-img src="/images/images2.jpg" sizes="sm:100vw md:50vw lg:400px" />
+<nuxt-img src="/images/images3.jpg" sizes="sm:100vw md:50vw lg:400px" />
 </div>
 </template>
